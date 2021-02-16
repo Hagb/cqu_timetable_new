c1200003fd89dc74032074c36317e22db31e85cc
Hagb Green <hagb_green@qq.com>
2021-02-16 16:07:22 +0800

    Refactor so that it can be called as a library

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ from openpyxl import load_workbook
 from icalendar import Calendar, Event, Timezone, vDDDTypes
 
 
-config = configparser.RawConfigParser()
-config.read_file(open('config.txt'))
-base_dir = config.get('config', 'base_dir')
-start_date = config.get('config', 'start_date')
-file_name = config.get('config', 'file_name')
+__all__ = ('mkevent', 'mkical', 'read_data')
 
 VTIMEZONE = Timezone.from_ical("""BEGIN:VTIMEZONE
 TZID:Asia/Shanghai
@@ -22,12 +18,6 @@ DTSTART:19700101T000000
 END:STANDARD
 END:VTIMEZONE""")
 TIMEZONE = VTIMEZONE.to_tz()
-
-year = start_date[0:4]
-month = start_date[4:6]
-day = start_date[6:]
-print(base_dir)
-dt = datetime.datetime(int(year), int(month), int(day), tzinfo=TIMEZONE)
 time_dict = {
     1: [(8, 30), (9, 15)],
     2: [(9, 25), (10, 10)],
@@ -59,18 +49,20 @@ def read_data(base_dir):
     ws = load_workbook(base_dir, read_only=True, data_only=True).worksheets[0]
     return tuple(ws.values)[2:]
 
+
 def split_range(string):
     range = string.split('-')
     return range if len(range) == 2 else range * 2
 
-def get_schedule(data):  # 返回值说明： ([开始周次，结束周次], ...)，星期几，是否整周，开始节数，结束节数
-    week_str, day_str = data[2].split('周') # 分隔周数和星期+节数
-    weeks = (split_range(week_range) for week_range in week_str.split(',')) # 解析周数
-    if day_str: # 非整周 day_str 为 "星期Xxx-xx节"
-        return weeks, day_str[2], False, * split_range(day_str[3:-1])
-    else: # 整周 day_str 为空
-        return weeks, '全', True, "1", "12"
 
+def get_schedule(data):  # 返回值说明： ([开始周次，结束周次], ...)，星期几，是否整周，开始节数，结束节数
+    week_str, day_str = data[2].split('周')  # 分隔周数和星期+节数
+    weeks = (split_range(week_range)
+             for week_range in week_str.split(','))  # 解析周数
+    if day_str:  # 非整周 day_str 为 "星期Xxx-xx节"
+        return weeks, day_str[2], False, * split_range(day_str[3:-1])
+    else:  # 整周 day_str 为空
+        return weeks, '全', True, "1", "12"
 
 
 def add_datetime(component, name, time):
@@ -85,7 +77,7 @@ def add_datetime(component, name, time):
     component.add(name, vdatetime)
 
 
-def mkcal(data, cal):
+def mkevent(data, cal, dt):
     weeks, week_day, all_week, start_class, end_class = get_schedule(data)
     event_class = Event()
     event_class.add('SUMMARY', data[0])
@@ -100,15 +92,18 @@ def mkcal(data, cal):
         if all_week is False:
             count = int(end_week) - int(start_week) + 1
             event.add("RRULE", {"freq": "weekly", "count": count})
-            class_start_date = dt + datetime.timedelta(weeks=int(start_week) - 1, days=week_dic[week_day] - 1)
+            class_start_date = dt + \
+                datetime.timedelta(weeks=int(start_week) - 1,
+                                   days=week_dic[week_day] - 1)
             class_start_time = datetime.timedelta(hours=time_dict[int(start_class)][0][0],
-                                                minutes=time_dict[int(start_class)][0][1])
+                                                  minutes=time_dict[int(start_class)][0][1])
             class_end_time = datetime.timedelta(hours=time_dict[int(end_class)][1][0],
                                                 minutes=time_dict[int(end_class)][1][1])
         else:
             class_start_time = datetime.timedelta(hours=8, minutes=30)
             class_end_time = datetime.timedelta(hours=21, minutes=35)
-            class_start_date = dt + datetime.timedelta(weeks=int(start_week) - 1)
+            class_start_date = dt + \
+                datetime.timedelta(weeks=int(start_week) - 1)
             count = (int(end_week) - int(start_week) + 1) * 7
             event.add("RRULE", {"freq": "daily", "count": count})
 
@@ -116,7 +111,7 @@ def mkcal(data, cal):
         dtend = class_start_date + class_end_time
         namespace = uuid.UUID(
             bytes=int(dtstart.timestamp()).to_bytes(length=8, byteorder='big') +
-                int(dtend.timestamp()).to_bytes(length=8, byteorder='big')
+            int(dtend.timestamp()).to_bytes(length=8, byteorder='big')
         )
 
         add_datetime(event, 'DTEND', dtend)
@@ -126,15 +121,30 @@ def mkcal(data, cal):
         event.add('DTSTAMP', datetime.datetime.now())
         cal.add_component(event)
 
-def main():
-    data = read_data(base_dir)
 
+def mkical(data, dt):
     cal = Calendar()
     cal.add('prodid', '-//CQU//CQU Calendar//')
     cal.add('version', '2.0')
     cal.add_component(VTIMEZONE)
     for items in data:
-        mkcal(items, cal)
+        mkevent(items, cal, dt)
+    return cal
+
+
+def main():
+    config = configparser.RawConfigParser()
+    config.read_file(open('config.txt'))
+    base_dir = config.get('config', 'base_dir')
+    start_date = config.get('config', 'start_date')
+    file_name = config.get('config', 'file_name')
+    year = start_date[0:4]
+    month = start_date[4:6]
+    day = start_date[6:]
+    dt = datetime.datetime(int(year), int(month), int(day), tzinfo=TIMEZONE)
+    data = read_data(base_dir)
+
+    cal = mkical(data, dt)
 
     f = open(file_name, 'wb')
     f.write(cal.to_ical())
