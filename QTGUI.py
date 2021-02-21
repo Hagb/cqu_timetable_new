ca88a7812b58f9710e44a0dc558cc1d04b48e941
weearc <qby19981121@gmail.com>
2021-02-21 22:34:30 +0800

    add Disclaimer

--- QTGUI.py
+++ QTGUI.py
@@ -23,10 +23,16 @@ class timetable_to_ics():
         QMessageBox.information(
             self.ui,
             "关于此程序",
-            "该程序是在 AGPLv3 下发布的自由软件，源码可在 https://github.com/weearc/cqu_timetable_new 获取，欢迎在遵守该协议的情况下自由运行、拷贝、分发、学习、修改该软件。\n\tCopyright (C) 2021 weearc\nThis program is free software: you can redistribute it and/or modify\n"
-            "it under the terms of the GNU Affero General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version."
-            "This program is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\nGNU Affero General Public License for more details.\n"
-            "You should have received a copy of the GNU Affero General Public License\nalong with this program.  If not, see <https://www.gnu.org/licenses/>."
+            "该程序是在 AGPLv3 下发布的自由软件，源码可在 https://github.com/weearc/cqu_timetable_new 获取，欢迎在遵守该协议的情况下自由运行、拷贝、分发、学习、修改该软件。\nCopyright (C) 2021 weearc\nThis program is free software: you can redistribute it and/or modify"
+            "it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version."
+            "This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details."
+            "You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>."
+        )
+        QMessageBox.warning(
+            self.ui,
+            "免责声明",
+            "由于教务系统导出错误、人为因素、或其他不可抗力导致的课表损坏、时间未对齐等状况我们只能深表遗憾，暂无任何解决方法。\n"
+            "Use at your own risk.（请自行承担使用风险）"
         )
         self.ui.BFileSelect.clicked.connect(lambda: self.file_select())
         self.ui.Bhelp.clicked.connect(lambda: self.show_help())
@@ -76,6 +82,7 @@ class timetable_to_ics():
             "关于",
             "请从登陆选课网站 http://my.cqu.edu.cn/enroll/ ，点击“查看课表”，再点击“Excel”下载课程表数据。点击下方的“OK”后选择下载下来的文件。\n"
             "选择文件并填写行课日期，点击 OK 开始生成课表文件。"
+
         )
 
 if __name__ == '__main__':
